48cf3ca1e81f3c6747d99f2a23ecfb2508e7c7af
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-12 14:16:28 -0400

    MedicinasDto.kt

--- app/src/main/java/edu/ucne/registrotecnicos/data/remote/dto/MedicinasDto.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/remote/dto/MedicinasDto.kt
@@ -2,7 +2,6 @@ package edu.ucne.registrotecnicos.data.remote.dto
 
 data class MedicinasDto(
     val medicinaId: Int,
-    val nombre: String? = null,
     val descripcion: String? = null,
     val monto: Double
 )
