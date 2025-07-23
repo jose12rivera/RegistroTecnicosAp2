462b09019dcfbb3497814bd9f0f683e780b204ab
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-23 19:50:00 -0400

    La notificacion de RegistroTecnicos.kt

--- app/src/main/java/edu/ucne/registrotecnicos/RegistroTecnicos.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/RegistroTecnicos.kt
@@ -2,8 +2,25 @@ package edu.ucne.registrotecnicos
 
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class RegistroTecnicos: Application() {
+    override fun onCreate() {         super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "notification_channel_id",
+                "Notificaciones Generales",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "Canal para notificaciones generales de la app"
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 }
