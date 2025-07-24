401e9224b48fd116e3b38e57f2f679dcd37aac6e
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-24 08:56:41 -0400

    ClienteApiModule.kt

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/di/ClienteApiModule.kt
@@ -0,0 +1,60 @@
+package edu.ucne.composedemo.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.composedemo.data.remote.ClienteApi
+import edu.ucne.registrotecnicos.data.remote.RemoteDataSource
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ClienteApiModule {
+
+    private const val BASE_URL = "https://ap2cliente-cadvhsa8bfdgcmb7.eastus2-01.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideClienteApi(moshi: Moshi, client: OkHttpClient): ClienteApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ClienteApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(clienteApi: ClienteApi): RemoteDataSource =
+        RemoteDataSource(clienteApi)
+}
