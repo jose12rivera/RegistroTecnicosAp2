cf6c6f02b360afbe0cb000d419081946487070b1
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-24 10:17:17 -0400

    ClienteEntity.kt

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/entity/ClienteEntity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/entity/ClienteEntity.kt
@@ -2,11 +2,20 @@ package edu.ucne.registrotecnicos.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import edu.ucne.registrotecnicos.data.remote.dto.ClienteDto
 
 @Entity(tableName = "Clientes")
 data class ClienteEntity(
     @PrimaryKey
-    val clienteId: Int? = null,
+    var clienteId: Int? = null,
     val nombres: String = "",
     val whatsApp: String = ""
 )
+
+fun ClienteEntity.toDto(): ClienteDto {
+    return ClienteDto(
+        clienteId = this.clienteId ?: 0,
+        nombres = this.nombres,
+        whatsApp = this.whatsApp
+    )
+}
