a0f67d769f21e9e9030f950e4f997d7c41302e5a
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-24 09:52:12 -0400

    ClienteApiModule.kt

--- app/src/main/java/edu/ucne/registrotecnicos/data/di/ClienteApiModule.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/di/ClienteApiModule.kt
@@ -1,5 +1,6 @@
 package edu.ucne.composedemo.di
 
+
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
@@ -7,7 +8,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.composedemo.data.remote.ClienteApi
-import edu.ucne.registrotecnicos.data.remote.RemoteDataSource
+import edu.ucne.registrotecnicos.data.di.MoshiCliente
+import edu.ucne.registrotecnicos.data.di.OkHttpClientCliente
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,36 +17,41 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
+
 @Module
+@InstallIn(SingletonComponent::class)
 object ClienteApiModule {
 
     private const val BASE_URL = "https://ap2cliente-cadvhsa8bfdgcmb7.eastus2-01.azurewebsites.net/"
 
     @Provides
     @Singleton
-    fun provideMoshi(): Moshi =
+    @MoshiCliente
+    fun provideMoshiCliente(): Moshi =
         Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
+    @OkHttpClientCliente
+    fun provideOkHttpClientCliente(): OkHttpClient {
         val loggingInterceptor = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
-
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
+            .readTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideClienteApi(moshi: Moshi, client: OkHttpClient): ClienteApi {
+    fun provideClienteApi(
+        @MoshiCliente moshi: Moshi,
+        @OkHttpClientCliente client: OkHttpClient
+    ): ClienteApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
@@ -52,9 +59,4 @@ object ClienteApiModule {
             .build()
             .create(ClienteApi::class.java)
     }
-
-    @Provides
-    @Singleton
-    fun provideRemoteDataSource(clienteApi: ClienteApi): RemoteDataSource =
-        RemoteDataSource(clienteApi)
 }
