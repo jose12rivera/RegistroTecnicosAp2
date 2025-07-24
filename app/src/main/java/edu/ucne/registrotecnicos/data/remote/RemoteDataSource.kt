0e78bb934246a63691cfdc77df9aeca1c4be3b99
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-24 08:58:11 -0400

    RemoteDataSource.kt

--- app/src/main/java/edu/ucne/registrotecnicos/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/remote/RemoteDataSource.kt
@@ -1,11 +1,14 @@
 package edu.ucne.registrotecnicos.data.remote
 
+import edu.ucne.composedemo.data.remote.ClienteApi
 import edu.ucne.composedemo.data.remote.MedicinasApi
+import edu.ucne.registrotecnicos.data.remote.dto.ClienteDto
 import edu.ucne.registrotecnicos.data.remote.dto.MedicinasDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
-    private val medicinasApi: MedicinasApi
+    private val medicinasApi: MedicinasApi,
+    private val clienteApi: ClienteApi
 ) {
     suspend fun getMedicinas(): List<MedicinasDto> = medicinasApi.getMedicinas()
 
@@ -16,4 +19,14 @@ class RemoteDataSource @Inject constructor(
     suspend fun updateMedicina(id: Int, medicina: MedicinasDto): MedicinasDto = medicinasApi.updateMedicina(id, medicina)
 
     suspend fun deleteMedicina(id: Int) = medicinasApi.deleteMedicina(id)
+
+    suspend fun getClientes(): List<ClienteDto> = clienteApi.getClientes()
+
+    suspend fun getCliente(id: Int): ClienteDto = clienteApi.getCliente(id)
+
+    suspend fun createCliente(cliente: ClienteDto): ClienteDto = clienteApi.createCliente(cliente)
+
+    suspend fun updateCliente(id: Int, cliente: ClienteDto): ClienteDto = clienteApi.updateCliente(id, cliente)
+
+    suspend fun deleteCliente(id: Int) = clienteApi.deleteCliente(id)
 }
